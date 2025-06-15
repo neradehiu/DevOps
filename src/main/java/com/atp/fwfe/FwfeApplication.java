3cab037edce26ac9549f8b459182ff61a31f5b8f
NGUYENVODUCCR <nguyenvoduc2004@gmail.com>
2025-06-15 15:54:19 +0700

    first commit

---
+++ src/main/java/com/atp/fwfe/FwfeApplication.java
@@ -0,0 +1,13 @@
+package com.atp.fwfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FwfeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FwfeApplication.class, args);
+	}
+
+}
